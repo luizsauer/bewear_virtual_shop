8fee07e4f592ebbe294645cf260869aad29495a1
luizsauer <luizsauer@gmail.com>
2025-08-08 22:32:09 -0300

    feed: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
-// import Footer from "@/components/common/footer";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -55,7 +55,7 @@ const Home = async () => {
         </div>
 
         <ProductList products={newlyCreatedProducts} title="Novos produtos" />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );
